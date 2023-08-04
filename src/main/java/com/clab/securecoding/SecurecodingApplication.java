dfcc8295cf582b0f9b73f3e08a8fc43b49b22730
LimeWindows <noop103@naver.com>
2023-08-04 17:27:32 +0900

    Project Initiation

---
+++ src/main/java/com/clab/securecoding/SecurecodingApplication.java
@@ -0,0 +1,13 @@
+package com.clab.securecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurecodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurecodingApplication.class, args);
+	}
+
+}
