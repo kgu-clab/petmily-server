faed24a572ac6fa199844a737aa60efa69a6bb84
Seokhyun Lee <lseokhyun99@gmail.com>
2023-08-14 13:04:51 +0900

    A: XSS filter 화이트리스트 추가

--- src/main/java/com/clab/securecoding/auth/filter/CrossSiteScriptingFilter.java
+++ src/main/java/com/clab/securecoding/auth/filter/CrossSiteScriptingFilter.java
@@ -2,6 +2,7 @@ package com.clab.securecoding.auth.filter;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
@@ -10,13 +11,30 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Arrays;
 
 @Slf4j
 @RequiredArgsConstructor
 public class CrossSiteScriptingFilter extends GenericFilterBean {
+
+    private static final String[] XSS_WHITELIST = {
+            "/resources/files/**"
+    };
+
+    private final AntPathMatcher antPathMatcher = new AntPathMatcher();
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        log.info("XSS filter");
-        chain.doFilter(new RequestWrapper((HttpServletRequest) request), response);
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        boolean isWhitelisted = Arrays.stream(XSS_WHITELIST)
+                .anyMatch(path -> antPathMatcher.match(path, requestURI));
+
+        if( isWhitelisted ){
+            chain.doFilter(request, response);
+        } else {
+            chain.doFilter(new RequestWrapper((HttpServletRequest) request), response);
+        }
     }
 }
